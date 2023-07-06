e2f32a230b4376121e760d7bdc87de9ac385b0b2
MysaelR <xxmysaelxx@gmail.com>
2023-07-06 10:17:58 -0300

    adicionando rotas

---
+++ aula/src/App.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import './style.css';
+
+
+
+function App() {
+
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        setNutri(json)
+      })
+    }
+
+    loadApi()
+  }, [])
+
+  return (
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+
+      </header>
+
+      {
+        nutri.map((item) => {
+          return(
+            <article className="posts" key={item.id}>
+              <strong className="titulo">{item.titulo}</strong>
+              <img className="capa" src={item.capa} alt={item.titulo}/>
+              <p className="subtitulo">
+                {item.subtitulo}
+              </p>
+              <a className="botao">Acessar</a>
+            </article>
+          )
+        })
+      }
+    
+    </div>
+  );
+}
+
+export default App;
